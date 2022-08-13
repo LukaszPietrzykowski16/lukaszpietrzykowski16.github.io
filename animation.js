7e7161c8257215ff6a049e8ea76a136219482114
Lukasz Pietrzykowski <lukaszpietrzykowsk@gmail.com>
2022-08-13 11:28:34 +0200

    new component

---
+++ animation.js
@@ -0,0 +1,27 @@
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/all';
+import { Power2 } from 'gsap/all';
+
+gsap.registerPlugin(ScrollTrigger)
+
+let appendPhotos = gsap.utils.toArray('.my-work-card');
+
+const myWorkDiv = document.querySelectorAll('.my-work-card');
+
+appendPhotos.forEach((card) => {
+    const image =  card.querySelector('.my-work-card'),
+    tl = gsap.timeline({paused: true});
+    tl.set(card, {autoAlpha: 0.1});
+    tl.from(card, 1.5, {
+        yPercent: 100,
+        ease: 'power2',
+        opacity: 0
+    });
+    card.animation = tl;
+})
+
+ScrollTrigger.batch(appendPhotos, {
+    onEnter: elements => elements.forEach((e, i) => 
+    e.animation.delay(i * 0.2).restart(true)),
+    once: true
+})
