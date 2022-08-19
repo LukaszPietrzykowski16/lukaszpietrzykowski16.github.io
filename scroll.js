9a25096bf199ddb8d4f8a12627a78c199e630e46
Lukasz Pietrzykowski <lukaszpietrzykowsk@gmail.com>
2022-08-19 11:26:10 +0200

    cleaning up repo

--- scroll.js
+++ scroll.js
@@ -4,17 +4,7 @@ const myWork = document.querySelector('.test-my-work');
 
 const technology = document.querySelector('.technology-animation');
 window.addEventListener('scroll', () => {
-    /*
-    let scrollingValue2 = window.scrollY * (-0.2);
-    threeD.style.transform = `translate3d(0px, ${scrollingValue2}px, 0px)`;
-    */
-
     let pos = window.scrollY;
     myWork.style.transform = `translateX(${pos - 800}px)`;
     technology.style.transform = `translateX(${-pos - 800}px)`;
- 
-    /*
-    let scrollingValue = window.scrollY * (0.2);
-    target.style.transform = `translate3d(0px, ${scrollingValue}px, 0px)`;
-  */
 })
