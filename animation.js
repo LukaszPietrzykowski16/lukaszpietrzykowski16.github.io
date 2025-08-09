baac5e740b1461c576cc2b3eac611985abb30edf
LukaszPietrzykowski16 <lukaszpietrzykowsk@gmail.com>
2025-08-09 22:47:15 +0200

    Updating my personal page

--- animation.js
+++ animation.js
@@ -1,40 +1,16 @@
-import { gsap } from 'gsap';
-import { ScrollTrigger } from 'gsap/all';
-import { Power2 } from 'gsap/all';
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/all";
 
-gsap.registerPlugin(ScrollTrigger)
+gsap.registerPlugin(ScrollTrigger);
 
-let appendPhotos = gsap.utils.toArray('.my-work-card');
+const myWorkDiv = document.querySelectorAll(".my-work-card");
 
-const myWorkDiv = document.querySelectorAll('.my-work-card');
-
-
-gsap.from(myWorkDiv, {scrollTrigger: {
+gsap.from(myWorkDiv, {
+  scrollTrigger: {
     trigger: myWorkDiv,
-    toggleActions: 'play none none none'},
-    y: 200,
-    duration: 1,
-    opacity: 0
-  })
-
-
-
-/*
-appendPhotos.forEach((card) => {
-    const image =  card.querySelector('.my-work-card'),
-    tl = gsap.timeline({paused: true});
-    tl.set(card, {autoAlpha: 0.6});
-    tl.from(card, 1.6, {
-        yPercent: 100,
-        ease: 'power2',
-        opacity: 0
-    });
-    card.animation = tl;
-})
-
-ScrollTrigger.batch(appendPhotos, {
-    onEnter: elements => elements.forEach((e, i) => 
-    e.animation.delay(i * 0.2).restart(true)),
-    once: true
-})
-*/
+    toggleActions: "play none none none",
+  },
+  y: 200,
+  duration: 1,
+  opacity: 0,
+});
--- firstPage.js
+++ firstPage.js
@@ -1,36 +1,33 @@
 import { gsap } from "gsap";
 
-const nameH1 = document.querySelector('.name h1')
+const nameH1 = document.querySelector(".name h1");
 
-
-window.addEventListener('DOMContentLoaded', () => {
-    underlineAnimation()
-})
+window.addEventListener("DOMContentLoaded", () => {
+  underlineAnimation();
+});
 
 function delay(time) {
-    return new Promise(resolve => setTimeout(resolve, time))
+  return new Promise((resolve) => setTimeout(resolve, time));
 }
 
-async function underlineAnimation(){
-	gsap.from(nameH1, {y:-100, duration:0.8, opacity:0})
-    await delay(1000);
-    nameH1.classList.toggle('under')
-    
+async function underlineAnimation() {
+  gsap.from(nameH1, { y: -100, duration: 0.8, opacity: 0 });
+  await delay(1000);
+  nameH1.classList.toggle("under");
 }
 
-const text = 'Hello I am frontend developer, who love learning new technologies and dive deep into new topics. So... That is it! If you scroll down you will see my work';
-
+const text =
+  "I am a developer with 3 years of experience specializing in Angular. Currently, I work for Envelo and am actively seeking new challenges that will allow me to grow my skills and work on ambitious projects.";
 
 var array = text.split("");
 var timer;
 
-async function frameLooper () {
-	if (array.length > 0) {
-		document.querySelector('.typed-text').innerHTML += array.shift();
-	} else {
-		clearTimeout(timer);
-			}
-	let loopTimer = setTimeout(frameLooper,30);  /* change 70 for speed */
-
+async function frameLooper() {
+  if (array.length > 0) {
+    document.querySelector(".typed-text").innerHTML += array.shift();
+  } else {
+    clearTimeout(timer);
+  }
+  let loopTimer = setTimeout(frameLooper, 30); /* change 70 for speed */
 }
 frameLooper();
